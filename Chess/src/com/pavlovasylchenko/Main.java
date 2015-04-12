665a3db4c2330ffda2f2031f939fc798d1224edc
PavloVasylchenko <Pavel.S.Vasilchenko@gmail.com>
2015-04-12 15:39:57 +0300

    Implement menu

--- Chess/src/com/pavlovasylchenko/Main.java
+++ Chess/src/com/pavlovasylchenko/Main.java
@@ -1,7 +1,6 @@
 package com.pavlovasylchenko;
 
-import java.util.Arrays;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
@@ -15,15 +14,14 @@ public class Main {
     // javac -d out src/com/**/*.java
     // java -cp out/ com.pavlovasylchenko.Main
     public static void main(String[] args) {
-        Engine engine = new Engine(7, 7, Arrays.asList(
-                Figure.QUEEN,
-                Figure.QUEEN,
-                Figure.KING,
-                Figure.KING,
-                Figure.BISHOP,
-                Figure.BISHOP,
-                Figure.KNIGHT
-        ));
+
+        ReadInput readInput = new ReadInput().invoke();
+
+        int width = readInput.getWidth();
+        int height = readInput.getHeight();
+        List<Figure> figures = readInput.getFigures();
+
+        Engine engine = new Engine(width, height, figures);
         long first = System.currentTimeMillis();
         Set<Field> results = engine.getResult(true);
         long last = System.currentTimeMillis();
@@ -36,28 +34,79 @@ public class Main {
 
     public static void printField(Figure[][] field) {
         System.out.println("-------------------------");
-        //int num = 0;
         for (Figure[] aField : field) {
             System.out.print("|");
             for (Figure anAField : aField) {
-                //num++;
                 if (anAField == Figure.KING) System.out.print("♚|");
                 if (anAField == Figure.QUEEN) System.out.print("♛|");
                 if (anAField == Figure.ROOK) System.out.print("♜|");
                 if (anAField == Figure.KNIGHT) System.out.print("♞|");
                 if (anAField == Figure.BISHOP) System.out.print("♝|");
-                if (anAField == Figure.NONE) {
-                    System.out.print("-|");
-                    //num--;
-                }
-                if (anAField == null) {
-                    System.out.print(" |");
-                    //num--;
-                }
+                if (anAField == Figure.NONE) System.out.print("-|");
+                if (anAField == null) System.out.print(" |");
             }
             System.out.println();
         }
-        //System.out.println(num);
         System.out.println("-------------------------");
+    }
+
+    private static class ReadInput {
+        private int width;
+        private int height;
+        private List<Figure> figures;
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public List<Figure> getFigures() {
+            return figures;
+        }
+
+        public ReadInput invoke() {
+            Scanner s = new Scanner(System.in);
+            System.out.println("Введите ширину поля: ");
+            width = s.nextInt();
+            System.out.println("Введите высоту поля: ");
+            height = s.nextInt();
+
+            System.out.println("Введите количество ферзей: ");
+            int queenCount = s.nextInt();
+
+            System.out.println("Введите количество ладей: ");
+            int rookCount = s.nextInt();
+
+            System.out.println("Введите количество офицеров: ");
+            int bishopCount = s.nextInt();
+
+            System.out.println("Введите количество коней: ");
+            int knightCount = s.nextInt();
+
+            System.out.println("Введите количество королей: ");
+            int kingCount = s.nextInt();
+
+            figures = new ArrayList<>();
+
+            for (int i = 0; i < queenCount; i++) {
+                figures.add(Figure.QUEEN);
+            }
+            for (int i = 0; i < rookCount; i++) {
+                figures.add(Figure.ROOK);
+            }
+            for (int i = 0; i < bishopCount; i++) {
+                figures.add(Figure.BISHOP);
+            }
+            for (int i = 0; i < knightCount; i++) {
+                figures.add(Figure.KNIGHT);
+            }
+            for (int i = 0; i < kingCount; i++) {
+                figures.add(Figure.KING);
+            }
+            return this;
+        }
     }
 }
